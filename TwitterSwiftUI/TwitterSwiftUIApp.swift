a13b0a139a4df6ebd479693f45fd20788c2b75b1
selcuk baki <selcukbaki@windowslive.com>
2021-11-02 04:27:11 +0300

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Selcuk Baki on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
